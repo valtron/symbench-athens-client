ff093103062d1963ac341644f5c25b56a612a2c1
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2021-08-15 06:04:17 -0500

    Remove pre-commit path patterns :shirt:. Closes #51 (#52)

--- bin/graph_data_exporter.py
+++ bin/graph_data_exporter.py
@@ -11,6 +11,7 @@ class ComponentCorpusExporter:
     gremlin_url: str
         The URL for the gremlin remote server (i.e. the JanusGraph server websocket address)
     """
+
     def __init__(self, gremlin_url, logger):
         self.gremlin_url = gremlin_url
         self.g = None
@@ -19,40 +20,65 @@ class ComponentCorpusExporter:
 
     def get_components(self):
         """Return a json serializable dictionary of properties for different components in the database"""
-        component_vertices = self.g.V().hasLabel("[avm]Component").values("[]Name").toList()
+        component_vertices = (
+            self.g.V().hasLabel("[avm]Component").values("[]Name").toList()
+        )
         all_components = {}
         components_with_missing_classes = []
         self.logger.info(f"Found {len(component_vertices)} components in the database")
         for component in component_vertices:
             # In favor of correctness and timing consideration, I have chosen the following queries
             # These queries might not be the most optimal/fastest
-            component_properties = self.g.V().hasLabel("[avm]Component").has("[]Name", component)\
-                .as_("comp").in_("inside")\
-                .hasLabel("[]Property")\
-                .as_("prop")\
-                .in_("inside")\
-                .in_("inside")\
-                .in_("inside")\
-                .in_("inside")\
-                .as_("val")\
-                .select("prop")\
-                .by("[]Name")\
-                .as_("propname")\
-                .select("val")\
-                .by(__.coalesce(__.values("value"), __.constant("none"))).as_("sval")\
-                .select("propname","sval").toList()
+            component_properties = (
+                self.g.V()
+                .hasLabel("[avm]Component")
+                .has("[]Name", component)
+                .as_("comp")
+                .in_("inside")
+                .hasLabel("[]Property")
+                .as_("prop")
+                .in_("inside")
+                .in_("inside")
+                .in_("inside")
+                .in_("inside")
+                .as_("val")
+                .select("prop")
+                .by("[]Name")
+                .as_("propname")
+                .select("val")
+                .by(__.coalesce(__.values("value"), __.constant("none")))
+                .as_("sval")
+                .select("propname", "sval")
+                .toList()
+            )
 
             all_components[component] = {}
 
-            component_class = self.g.V().hasLabel("[avm]Component")\
-                .has("[]Name", component).in_().hasLabel("[]Classifications").in_().as_("class").select("class").by("value").toList()
+            component_class = (
+                self.g.V()
+                .hasLabel("[avm]Component")
+                .has("[]Name", component)
+                .in_()
+                .hasLabel("[]Classifications")
+                .in_()
+                .as_("class")
+                .select("class")
+                .by("value")
+                .toList()
+            )
 
-            self.logger.info(f"Extracted properties for {component}. The component type is {component_class}")
+            self.logger.info(
+                f"Extracted properties for {component}. The component type is {component_class}"
+            )
 
             for component_property in component_properties:
-                all_components[component][component_property['propname']] = component_property['sval']
+                all_components[component][
+                    component_property["propname"]
+                ] = component_property["sval"]
 
-            all_components[component]["Classification"] = component_class[0] if len(component_class) else None
+            all_components[component]["Classification"] = (
+                component_class[0] if len(component_class) else None
+            )
 
             if not len(component_class):
                 self.logger.warn(f"Missing component classification for {component}")
@@ -75,13 +101,14 @@ class ComponentCorpusExporter:
         self.logger.info("Closed connection to the gremlin server")
 
     def connect(self):
-        self.connection = DriverRemoteConnection(self.gremlin_url, 'g')
+        self.connection = DriverRemoteConnection(self.gremlin_url, "g")
         self.g = traversal().withRemote(self.connection)
         self.logger.info(f"Connected to gremlin server at {self.gremlin_url}")
 
 
 if __name__ == "__main__":
     import json
+
     from symbench_athens_client.utils import get_data_file_path, get_logger
 
     GREMLIN_URL = "ws://localhost:8182/gremlin"
@@ -91,9 +118,11 @@ if __name__ == "__main__":
     logger = get_logger(f"{__file__}::{__name__}::{ComponentCorpusExporter.__name__}")
     with ComponentCorpusExporter(GREMLIN_URL, logger=logger) as exporter:
         components, missing = exporter.get_components()
-        with open(OUT_FILE, 'w') as json_file:
+        with open(OUT_FILE, "w") as json_file:
             json.dump(components, json_file, indent=2)
 
         with open(MISSING_PATH, "w") as missing_classifications_file:
-            missing_classifications_file.write("##Note: These components are missing classes in the database:\n")
+            missing_classifications_file.write(
+                "##Note: These components are missing classes in the database:\n"
+            )
             missing_classifications_file.write(",".join(missing))
