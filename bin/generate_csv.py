bdf5d2fdbe42db9ff9182c23216fd98500b3fe08
Peter Volgyesi <peter.volgyesi@gmail.com>
2021-08-18 14:33:10 -0500

    Bugfix in CSV export

--- bin/generate_csv.py
+++ bin/generate_csv.py
@@ -13,5 +13,5 @@ from symbench_athens_client.models.components import (
 if __name__ == "__main__":
 
     for cls in Battery, Motor, Propeller, Wing:
-        builder = ComponentsBuilder(Battery, get_all_components_of_class(Battery))
+        builder = ComponentsBuilder(cls, get_all_components_of_class(Battery))
         builder.to_csv(f"{cls.__name__}.csv")
