3399eb46fc092601d1074f79a3e1f4fd7e6f3735
Peter Volgyesi <peter.volgyesi@gmail.com>
2021-08-18 14:29:13 -0500

    Add CSV export tool

---
+++ bin/generate_csv.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Generate CSV files (one per component type) from the database"""
+from symbench_athens_client.models.components import (
+    get_all_components_of_class,
+    ComponentsBuilder,
+    Battery,
+    Motor,
+    Propeller,
+    Wing,
+)
+
+
+if __name__ == "__main__":
+
+    for cls in Battery, Motor, Propeller, Wing:
+        builder = ComponentsBuilder(Battery, get_all_components_of_class(Battery))
+        print(cls.__name__)
+        builder.to_csv(f"{cls.__name__}.csv")
