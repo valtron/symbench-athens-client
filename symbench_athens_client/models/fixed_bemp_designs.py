c96e0d00805aec435cf3aedb99988a7bcb7bd8c5
Peter Volgyesi <peter.volgyesi@gmail.com>
2021-08-26 11:32:44 -0500

    Add new fixed design (QuadCopter_5)

--- symbench_athens_client/models/fixed_bemp_designs.py
+++ symbench_athens_client/models/fixed_bemp_designs.py
@@ -42,3 +42,24 @@ class TurnigyGraphene6000MAHQuadCopter(QuadCopter):
         self.propeller_3 = Propellers.apc_propellers_16x4E
 
         self.validate_propellers_directions()
+
+
+class QuadCopter_5(QuadCopter):
+    def __init__(self, *args, **kwargs):
+        super(QuadCopter_5, self).__init__(*args, **kwargs)
+        # Change Battery
+        self.battery_0 = Batteries.TurnigyGraphene6000mAh6S75C
+
+        # Change Motors
+        self.motor_0 = Motors.t_motor_MN5208KV340
+        self.motor_1 = Motors.t_motor_MN5208KV340
+        self.motor_2 = Motors.t_motor_MN5208KV340
+        self.motor_3 = Motors.t_motor_MN5208KV340
+
+        # Change Propellers
+        self.propeller_0 = Propellers.apc_propellers_18x5_5MRP
+        self.propeller_1 = Propellers.apc_propellers_18x5_5MR
+        self.propeller_2 = Propellers.apc_propellers_18x5_5MRP
+        self.propeller_3 = Propellers.apc_propellers_18x5_5MR
+
+        self.validate_propellers_directions()
